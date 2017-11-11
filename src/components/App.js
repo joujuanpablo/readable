e6e5d78ccf2c196c5fde0c262588dd79a27ae5b1
Jou-Valencia, Juan Pablo (UK - London) <joujuanpablo@gmail.com>
2017-11-11 11:03:35 +0000

    feature: setting up react-bootstrap
    * Including stylesheet
    * Importing Routers
    * Renaming nav component
    * Building up the navbar
    
    footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import { Route } from 'react-router-dom';
+
+import Navbar from './Navbar'
 
 class App extends Component {
+  state= {
+      categories: ['Politics', 'Art', 'Music', 'Science', 'Sports'],
+    }
+    
   render() {
     return (
       <div className="App">
@@ -10,6 +17,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Readable</h1>
         </header>
+        <Navbar></Navbar>
       </div>
     );
   }
