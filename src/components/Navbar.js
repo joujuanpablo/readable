755cb57f8180503d36611141485959d229c7799f
Jou-Valencia, Juan Pablo (UK - London) <joujuanpablo@gmail.com>
2017-11-13 10:17:22 +0000

    feature: Set up app state by install dependencies and creating store and set up API methods * Install redux, redux-thunk, react-redux, and create reducer file (using combine reducers from redux) * Create api.js and build up methods using fetch * Apply categories method to get categories for the nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as ReadableAPI from '../utils/api';
 
 
 class Navbar extends Component {
+    state = {
+        categories: [],
+    }
+
+    componentDidMount() {
+        ReadableAPI.getCategories().then((categories) => {
+            const categoryNames = categories.map((category) => category.name)
+            console.log(categoryNames)
+            this.setState({ categories: categoryNames });
+        })
+    }
 
     render() {
         return (
             <nav className='navbar navbar-expand-lg navbar-light bg-light rounded'>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-md-center" id='navbarsExample10'>
                     <ul className='navbar-nav' onSelect={this.handleSelect} >
                         {
-                            this.props.categories.map((category) => (
-                                <li eventKey={category} className='nav-item'><Link className='nav-link' to={`/${category}`}>{category}</Link></li>
+                            this.state.categories.map((category) => (
+                                <li key={category} className='nav-item'><Link className='nav-link' to={`/${category}`}>{category}</Link></li>
                             ))
                         }
-                        <li eventKey="create" className='nav-item'><Link className='nav-link' to='/create'>Create Post</Link></li>
+                        <li key="create" className='nav-item'><Link className='nav-link' to='/create'>Create Post</Link></li>
                     </ul >
                 </div>
             </nav>
