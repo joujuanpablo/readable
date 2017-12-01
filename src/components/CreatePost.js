81a4e175e918fb9bae7032919f3f2bc526431e45
Jou-Valencia, Juan Pablo (UK - London) <joujuanpablo@gmail.com>
2017-12-01 16:44:10 +0000

    feat: scaffold out the post form component
    * create a form styled with bootstrap
    * placeholder text is populated as props so that it can be populated dynamically, however the post would have to be rewritten if it was being edited so this needs refactoring

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import PostForm from './PostForm'
 
 class CreatePost extends Component {
     render(){
         return(
-            <div className='create-post'>CREATE POST PLACEHOLDER</div>
+            <div className='create-post'>
+                <PostForm title='title' author='author' content='..write your post here'/>
+            </div>
         )
     }
 }
---
+++ src/components/PostForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    render() {
+        const { title, author, content, category } = this.props
+        return (
+            <div className='post-form container'>
+                <form action="">
+                    <div className='form-group'>
+                        <label htmlFor="postTitle">Post title</label>
+                        <input className='form-control post-form--title post-form-field' type="text" placeholder={title} />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="postAuthor">Post author</label>
+                        <input className='form-control post-form--author post-form-field' type="text" placeholder={author} />
+                        <small className='form-text text-muted'>This can be whatever you like since this is an annonymous app.</small>
+                    </div>
+
+                    <div className='form-group'>
+                    <label htmlFor="postCategory">Post category</label>
+                        <select className='form-control post-form--category post-form-field' value={category} type="text" placeholder=''>
+                            <option value="">Please Choose a Category</option>
+                            <option value="react">React</option>
+                            <option value="redux">Redux</option>
+                            <option value="Udacity">Udacity</option>
+                        </select>
+                    </div>
+                    <div className='form-group'>
+                    <label htmlFor="postBody">The post</label>
+                        <textarea className='form-control post-form--content form-control' type="text" placeholder={content} />
+                    </div>
+                    <div className='form-group'>
+                        <button className='form-control post-form-field'>Post</button>
+                    </div>
+
+                </form>
+            </div>
+
+        )
+    }
+}
+
+export default PostForm 
